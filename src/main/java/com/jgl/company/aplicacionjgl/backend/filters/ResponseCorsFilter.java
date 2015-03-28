d21a8f9ba6dd1b5eb3f59b30a33520836ed031c4
jhonatan <>
2015-03-28 10:04:28 -0500

    Cors by ResponseCORSFilters

---
+++ src/main/java/com/jgl/company/aplicacionjgl/backend/filters/ResponseCorsFilter.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jgl.company.aplicacionjgl.backend.filters;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class ResponseCorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT, DELETE");
+        
+    }
+
+}
--- src/main/java/com/jgl/company/aplicacionjgl/backend/service/WebApp.java
+++ src/main/java/com/jgl/company/aplicacionjgl/backend/service/WebApp.java
@@ -1,11 +1,15 @@
 package com.jgl.company.aplicacionjgl.backend.service;
 
+import com.jgl.company.aplicacionjgl.backend.filters.ResponseCorsFilter;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("/webresources")
 public class WebApp extends ResourceConfig {
-	public WebApp(){
+
+    public WebApp() {
         packages("com.jgl.company.aplicacionjgl.backend.service");
+        // register filters
+        register(ResponseCorsFilter.class);
     }
 }
