e528640f534df808f1493afd949a4d9881cf90a8
Jhonatan <jhonatan.alarcon.ariza@gmail.com>
2015-04-09 16:36:11 -0500

    Se agrega la excepcion para el metodo de login

--- src/main/java/com/jgl/company/aplicacionjgl/backend/persistence/UsuarioPersistence.java
+++ src/main/java/com/jgl/company/aplicacionjgl/backend/persistence/UsuarioPersistence.java
@@ -9,6 +9,8 @@ import com.jgl.company.aplicacionjgl.backend.DTO.LoginDTO;
 import com.jgl.company.aplicacionjgl.backend.entity.UsuarioEntity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.ws.rs.core.Response;
@@ -27,26 +29,26 @@ public class UsuarioPersistence {
     }
     
     
-    public Response login(LoginDTO login) {
-        int status = 500;
+    public Response login(LoginDTO login) throws NonUniqueResultException  { //Excepción que garantiza un solo resultado
         String statusStr = "usuario autenticado";
         entityManager = emf.createEntityManager();
         UsuarioEntity user = null;
-        try {
+        try{
             entityManager.getTransaction().begin();
             Query q = entityManager.createQuery("select u from UsuarioEntity u where u.userName=:usern AND u.pass=:pass",UsuarioEntity.class); //debe ser u.userName con cast al final del query
             q.setParameter("usern", login.getUserName()).setParameter("pass", login.getPass());
             user = (UsuarioEntity) q.getSingleResult();
             entityManager.getTransaction().commit();
-            status = 200;
-        } catch (Exception e) {
+            return Response.status(200).entity(statusStr).build();
+        }catch(NoResultException e){
             entityManager.close();
-            e.printStackTrace();
             statusStr = "usuario no autenticado";
+            return Response.status(200).entity(statusStr).build(); 
+        }    
             
-        }
+            
         
-        return Response.status(status).entity(statusStr).build();
+        
     }
 
 }
